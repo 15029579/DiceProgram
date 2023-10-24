77b12d34ed4a577d00a16ab2a37a88955a4f9012
15029579 <126074898+15029579@users.noreply.github.com>
2023-10-24 12:50:05 +0100

    Added static variables

--- src/diceprogram/DiceProgram.java
+++ src/diceprogram/DiceProgram.java
@@ -7,9 +7,21 @@ package diceprogram;
 public class DiceProgram {
 
     public static void main(String[] args) {
+        
+        Dice.setNumberOfFaces(24);
+        
         Dice player1 = new Dice();
-        System.out.println(player1.getCurrentShownFace());
-        System.out.println(player1.rollDice());
+        Dice player2 = new Dice();
+        
+        System.out.println(player1.getNumberOfFaces());
+        System.out.println(player2.getNumberOfFaces());
+        
+        player1.setNumberOfFaces(12);
+        
+        System.out.println(player1.getNumberOfFaces());
+        System.out.println(player2.getNumberOfFaces());
+//        System.out.println(player1.getCurrentShownFace());
+//        System.out.println(player1.rollDice());
     }
     
 }
